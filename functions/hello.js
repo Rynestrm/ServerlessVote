e5d1bc200dbc0aad59df396ca166237b06a833c5
Ivan <gibson.ivan@gmail.com>
2020-10-19 17:23:47 -0400

    code update

--- functions/hello.js
+++ functions/hello.js
@@ -1,9 +1,21 @@
-exports.handler = async (event) => {
-  const subject = event.queryStringParameters.name || 'World';
+const votesArray = [0, 0, 0, 0];
 
+exports.handler = async (event) => {
+  const v = event.queryStringParameters.vote;
+  if (v === 'a') {
+    votesArray[0] += 1;
+  } else if (v === 'b') {
+    votesArray[1] += 1;
+  } else if (v === 'c') {
+    votesArray[2] += 1;
+  } else if (v === 'd') {
+    votesArray[3] += 1;
+  } else if (v === 'r') {
+    votesArray[(0, 0, 0, 0)];
+  }
   return {
     statusCode: 200,
-
-    body: `Hello ${subject}!`,
+    header: { 'Allow-Control-Allow-Origin': '*' },
+    body: JSON.stringify(votesArray),
   };
 };

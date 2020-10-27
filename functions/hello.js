4555b89382976953d3290866634de5d58429e3ff
Ivan <gibson.ivan@gmail.com>
2020-10-27 13:29:27 -0400

    changing stuff

--- functions/hello.js
+++ functions/hello.js
@@ -1,23 +1,19 @@
-const votesArray = [0, 0, 0, 0];
+const positonArray = [0, 0];
 
 exports.handler = async (event) => {
-  const v = event.queryStringParameters.vote;
-  if (v === 'a') {
-    votesArray[0] += 1;
-  } else if (v === 'b') {
-    votesArray[1] += 1;
-  } else if (v === 'c') {
-    votesArray[2] += 1;
-  } else if (v === 'd') {
-    votesArray[3] += 1;
-  } else if (v === 'r') {
-    votesArray[0, 0, 0, 0];
-  }
+  const lat = event.queryStringParameters.lat;
+  const lng = event.queryStringParameters.lng;
+  postionArray[0] = lat;
+  console.log(lat);
+//   if (lat) {
+//   } else if {
+
+//   }
   console.log(JSON.stringify(votesArray));
 
   return {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(votesArray),
+    body: JSON.stringify(positonArray),
   };
 };
